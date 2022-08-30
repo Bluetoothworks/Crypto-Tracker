4fbffd549ed53b0b09be0d4949d40d1a781ddec0
Bluetoothworks <rik200126@gmail.com>
2022-08-30 15:52:21 +0530

    first commit

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import ReactRouterDOM from 'react-router-dom';
+function Navbar(){
+  return(
+    <section>
+      <label className='logo'>Crypto Tracker</label>
+      <ul>
+            <li><a class="active" href="#">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Feedback</a></li>
+        </ul>
+    </section>
+  );
+}
+  export default Navbar;
