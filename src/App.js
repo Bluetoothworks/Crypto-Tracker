6ada4143020c3660fc2cba3d766950d37963244d
m-ayan-k <mycoolfriends098@gmail.com>
2022-08-31 19:11:56 +0530

    added fetchcoinsdata function in api folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Navbar from "./components/navbar/navbar";
+import { fetchcoinsdata } from './api';
+
 function App() {
+  const [coinData,setcoinData]=useState([]);
+  useEffect(()=>{
+    
+    const fetchAPI= async ()=>{
+      const data=await fetchcoinsdata();
+      // console.log(data);
+      setcoinData(data);
+    }
+    fetchAPI();
+  },[]);
   return (
     <div id="home">
       <Navbar />
---
+++ src/api/index.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+export const fetchcoinsdata= async (currency="usd")=>{
+    const url=`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
+    try {
+        const {data}= await axios.get(url);
+        console.log(data);
+        const data_need=data.map((coin_info)=>({
+            id:coin_info.id,
+            symbol:coin_info.symbol,
+            name:coin_info.name,
+            image:coin_info.image,
+            price:coin_info.current_price,
+            change_24h:coin_info.market_cap_change_percentage_24h,
+            market_cap:coin_info.market_cap
+        }));
+        // console.log(data);
+        return data_need;
+        
+    } catch (error) {
+        console.log("Error");
+    }
+}
+
+export const singlecoin= async (id) => {
+    const url=`https://api.coingecko.com/api/v3/coins/${id}`;
+    
+}
+
+export const HistoricalChart= async(id,days=365,currency) =>{
+    const url=`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
+}
