9a341259f8ddc16d3669c81fe2b2e84b158e5597
Bluetoothworks <rik200126@gmail.com>
2022-09-01 11:54:08 +0530

    some frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Navbar from "./components/navbar/navbar";
+import List from "./components/list/list";
+import './styles.css';
+
 function App() {
   return (
     <div id="home">
       <Navbar />
+      <List />
     </div>
   );
 }
