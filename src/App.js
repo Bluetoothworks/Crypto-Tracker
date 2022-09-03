60bc58937b89b138ee52ca164699b2143563ddda
Tushar <jhamtushar12@gmail.com>
2022-09-03 23:18:27 +0530

    Added Main folder and Data Component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/navbar/navbar";
 import List from "./components/list/list";
-import './styles.css';
-import { fetchcoinsdata } from './api';
-
+import "./styles.css";
+import { fetchcoinsdata } from "./api";
+import Main from "./components/main/Main";
 
 function App() {
-  const [coinData,setcoinData]=useState([]);
-  useEffect(()=>{
-    
-    const fetchAPI= async ()=>{
-      const data=await fetchcoinsdata();
+  const [coinData, setcoinData] = useState([]);
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const data = await fetchcoinsdata();
       // console.log(data);
       setcoinData(data);
-    }
+    };
     fetchAPI();
-  },[]);
+  }, []);
   return (
     <div id="home">
       <Navbar />
       <List />
+      <Main />
     </div>
   );
 }
 
-
 export default App;
-
-
---
+++ src/components/main/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Data from "./Data";
+import "./Main.css";
+const Main = () => {
+  return (
+    <div id="main">
+      <Data />
+    </div>
+  );
+};
+
+export default Main;
